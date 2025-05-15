b85072f7faff39408c692a8012e37cc0bd79881d
taoaustin <taoaustin@yahoo.ca>
2025-05-15 11:51:54 -0400

    save on write

--- arch/nvim/after/plugin/lspconfig.lua
+++ arch/nvim/after/plugin/lspconfig.lua
@@ -30,3 +30,9 @@ lspconfig["haxe_language_server"].setup({
     capabilities = capabilities
 })
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = buffer,
+    callback = function ()
+        vim.lsp.buf.format {async = true}
+    end
+})
