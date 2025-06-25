a629d967d0a7ec521ef0322c834d216170193429
taoaustin <taoaustin@yahoo.ca>
2025-06-24 21:52:32 -0400

    awesome changes

--- arch/awesome/rc.lua
+++ arch/awesome/rc.lua
@@ -16,29 +16,34 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
+	naughty.notify({
+		preset = naughty.config.presets.critical,
+		title = "Oops, there were errors during startup!",
+		text = awesome.startup_errors,
+	})
 end
 
 -- Handle runtime errors after startup
 do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
+	local in_error = false
+	awesome.connect_signal("debug::error", function(err)
+		-- Make sure we don't go into an endless error loop
+		if in_error then
+			return
+		end
+		in_error = true
 
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
+		naughty.notify({
+			preset = naughty.config.presets.critical,
+			title = "Oops, an error happened!",
+			text = tostring(err),
+		})
+		in_error = false
+	end)
 end
 -- }}}
 
@@ -57,61 +62,70 @@ terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
-
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile.left,
-    awful.layout.suit.floating,
-    -- awful.layout.suit.tile,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
+	awful.layout.suit.tile.left,
+	awful.layout.suit.floating,
+	-- awful.layout.suit.tile,
+	-- awful.layout.suit.tile.bottom,
+	-- awful.layout.suit.tile.top,
+	-- awful.layout.suit.fair,
+	-- awful.layout.suit.fair.horizontal,
+	-- awful.layout.suit.spiral,
+	-- awful.layout.suit.spiral.dwindle,
+	-- awful.layout.suit.max,
+	-- awful.layout.suit.max.fullscreen,
+	-- awful.layout.suit.magnifier,
+	-- awful.layout.suit.corner.nw,
+	-- awful.layout.suit.corner.ne,
+	-- awful.layout.suit.corner.sw,
+	-- awful.layout.suit.corner.se,
 }
 -- }}}
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
-   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end },
+	{
+		"hotkeys",
+		function()
+			hotkeys_popup.show_help(nil, awful.screen.focused())
+		end,
+	},
+	{ "manual", terminal .. " -e man awesome" },
+	{ "edit config", editor_cmd .. " " .. awesome.conffile },
+	{ "restart", awesome.restart },
+	{
+		"quit",
+		function()
+			awesome.quit()
+		end,
+	},
 }
 
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
+mymainmenu = awful.menu({
+	items = {
+		{ "awesome", myawesomemenu, beautiful.awesome_icon },
+		{ "open terminal", terminal },
+	},
+})
 
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
-
 local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
+	-- Wallpaper
+	if beautiful.wallpaper then
+		local wallpaper = beautiful.wallpaper
+		-- If wallpaper is a function, call it with the screen
+		if type(wallpaper) == "function" then
+			wallpaper = wallpaper(s)
+		end
+		gears.wallpaper.maximized(wallpaper, s, true)
+	end
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
@@ -119,14 +133,12 @@ screen.connect_signal("property::geometry", set_wallpaper)
 
 local bars = require("wibars")
 
-awful.screen.connect_for_each_screen(
-    function(s)
-        -- Wallpaper
-        set_wallpaper(s)
-        -- Creating the taskbar for each screen
-        bars.create(s)
-    end
-)
+awful.screen.connect_for_each_screen(function(s)
+	-- Wallpaper
+	set_wallpaper(s)
+	-- Creating the taskbar for each screen
+	bars.create(s)
+end)
 
 -- Set global & client keys & buttons
 require("keys")
--- arch/awesome/wibars.lua
+++ arch/awesome/wibars.lua
@@ -10,300 +10,316 @@ local bars = {}
 -- Create a textclock widget
 local mytextclock = wibox.widget.textclock("%F | %H:%M")
 
-
 local bat = lain.widget.bat({
-    battery = "BAT0",
-    settings =
-        function()
-            --local perc = bat_now.perc ~= "N/A" and bat_now.perc .. "%" or bat_now.perc
-            local perc = bat_now.perc .. "%"
-            if bat_now.ac_status == 1 then
-                perc = " " .. perc
-            end
-            widget:set_markup(perc)
-        end,
-    full_notify = "off",
+	battery = "BAT0",
+	settings = function()
+		--local perc = bat_now.perc ~= "N/A" and bat_now.perc .. "%" or bat_now.perc
+		local perc = bat_now.perc .. "%"
+		if bat_now.ac_status == 1 then
+			perc = " " .. perc
+		end
+		widget:set_markup(perc)
+	end,
+	full_notify = "off",
 })
 
 local cpu = lain.widget.cpu({
-    settings =
-        function ()
-            local usage = string.format(" :%2d%%", cpu_now.usage)
-            widget:set_markup(usage)
-        end
+	settings = function()
+		local usage = string.format(" :%2d%%", cpu_now.usage)
+		widget:set_markup(usage)
+	end,
 })
 
 local cpu_temp = lain.widget.temp({
-    settings =
-        function ()
-            local temp = string.format("(%2d󰔄)", coretemp_now)
-            widget:set_markup(temp)
-        end
+	settings = function()
+		local temp = string.format("(%2d󰔄)", coretemp_now)
+		widget:set_markup(temp)
+	end,
 })
 
 local taglist_buttons = gears.table.join(
-    awful.button({}, 1, function(t) t:view_only() end),
-    awful.button({modkey}, 1, 
-        function(t)
-            if client.focus then
-                client.focus:move_to_tag(t)
-            end
-        end
-    ),
-    awful.button({}, 3, awful.tag.viewtoggle),
-    awful.button({modkey}, 3, 
-        function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ),
-    awful.button({}, 4, function(t) awful.tag.viewnext(t.screen) end),
-    awful.button({}, 5, function(t) awful.tag.viewprev(t.screen) end)
+	awful.button({}, 1, function(t)
+		t:view_only()
+	end),
+	awful.button({ modkey }, 1, function(t)
+		if client.focus then
+			client.focus:move_to_tag(t)
+		end
+	end),
+	awful.button({}, 3, awful.tag.viewtoggle),
+	awful.button({ modkey }, 3, function(t)
+		if client.focus then
+			client.focus:toggle_tag(t)
+		end
+	end),
+	awful.button({}, 4, function(t)
+		awful.tag.viewnext(t.screen)
+	end),
+	awful.button({}, 5, function(t)
+		awful.tag.viewprev(t.screen)
+	end)
 )
 
 local tasklist_buttons = gears.table.join(
-    awful.button({}, 1, 
-        function (c)
-            if c == client.focus then
-                c.minimized = true
-            else
-                c:emit_signal(
-                    "request::activate",
-                    "tasklist",
-                    {raise = true}
-                )
-            end
-        end
-    ),
-    awful.button({}, 3, 
-        function()
-            awful.menu.client_list({theme = {width = 250}})
-        end
-    ),
-    awful.button({}, 4, function () awful.client.focus.byidx(1) end),
-    awful.button({}, 5, function () awful.client.focus.byidx(-1) end)
+	awful.button({}, 1, function(c)
+		if c == client.focus then
+			c.minimized = true
+		else
+			c:emit_signal("request::activate", "tasklist", { raise = true })
+		end
+	end),
+	awful.button({}, 3, function()
+		awful.menu.client_list({ theme = { width = 250 } })
+	end),
+	awful.button({}, 4, function()
+		awful.client.focus.byidx(1)
+	end),
+	awful.button({}, 5, function()
+		awful.client.focus.byidx(-1)
+	end)
 )
 
-
 function bars.create(s)
-    -- Each screen has its own tag table.
-    awful.tag({ "1",  "2", "3", "4", "5"}, s, awful.layout.layouts[1])
+	-- Each screen has its own tag table.
+	awful.tag({ "1", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
 
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating pwhich layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-            awful.button({}, 1, function() awful.layout.inc(1) end),
-            awful.button({}, 3, function() awful.layout.inc(-1) end),
-            awful.button({}, 4, function() awful.layout.inc(1) end),
-            awful.button({}, 5, function() awful.layout.inc(-1) end)
-        )
-    )
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons,
-        style = {shape = function(cr,w,h) gears.shape.rounded_rect(cr,w,h,8) end},
-        layout = {layout = wibox.layout.flex.horizontal},
-        widget_template =
-        {
-            {
-                {
-                    id = "text_role",
-                    widget = wibox.widget.textbox
-                },
-                widget = wibox.container.place
-            },
-            id = "background_role",
-            widget = wibox.container.background
-        }
-    }
+	-- Create a promptbox for each screen
+	s.mypromptbox = awful.widget.prompt({})
+	-- Create an imagebox widget which will contain an icon indicating pwhich layout we're using.
+	-- We need one layoutbox per screen.
+	s.mylayoutbox = awful.widget.layoutbox(s)
+	s.mylayoutbox:buttons(gears.table.join(
+		awful.button({}, 1, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 3, function()
+			awful.layout.inc(-1)
+		end),
+		awful.button({}, 4, function()
+			awful.layout.inc(1)
+		end),
+		awful.button({}, 5, function()
+			awful.layout.inc(-1)
+		end)
+	))
+	-- Create a taglist widget
+	s.mytaglist = awful.widget.taglist({
+		screen = s,
+		filter = awful.widget.taglist.filter.all,
+		buttons = taglist_buttons,
+		style = {
+			shape = function(cr, w, h)
+				gears.shape.rounded_rect(cr, w, h, 8)
+			end,
+		},
+		layout = { layout = wibox.layout.flex.horizontal },
+		widget_template = {
+			{
+				{
+					id = "text_role",
+					widget = wibox.widget.textbox,
+				},
+				widget = wibox.container.place,
+			},
+			id = "background_role",
+			widget = wibox.container.background,
+		},
+	})
 
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons,
-        style = {shape = function(cr,w,h) gears.shape.rounded_rect(cr,w,h,4) end},
-        layout = {
-            max_widget_size = 35,
-            layout = wibox.layout.flex.horizontal},
-        widget_template =
-        {
-            {
-                {
-                    {
-                        {
-                            id = "icon_role",
-                            widget = wibox.widget.imagebox
-                        },
-                        {
-                            id = "text_role",
-                            widget = wibox.widget.textbox
-                        },
-                        layout = wibox.layout.fixed.horizontal
-                    },
-                    widget = wibox.container.place
-                },
-                margins = 2,
-                widget = wibox.container.margin
-            },
-            id = "background_role",
-            widget = wibox.container.background,
-        }
-    }
+	-- Create a tasklist widget
+	s.mytasklist = awful.widget.tasklist({
+		screen = s,
+		filter = awful.widget.tasklist.filter.currenttags,
+		buttons = tasklist_buttons,
+		style = {
+			shape = function(cr, w, h)
+				gears.shape.rounded_rect(cr, w, h, 4)
+			end,
+		},
+		layout = {
+			max_widget_size = 35,
+			layout = wibox.layout.flex.horizontal,
+		},
+		widget_template = {
+			{
+				{
+					{
+						{
+							id = "icon_role",
+							widget = wibox.widget.imagebox,
+						},
+						{
+							id = "text_role",
+							widget = wibox.widget.textbox,
+						},
+						layout = wibox.layout.fixed.horizontal,
+					},
+					widget = wibox.container.place,
+				},
+				margins = 2,
+				widget = wibox.container.margin,
+			},
+			id = "background_role",
+			widget = wibox.container.background,
+		},
+	})
 
-    s.mytags = wibox {
-        screen = s,
-        width = 100,
-        height = 25,
-        x = s.geometry.x + 2 * beautiful.useless_gap,
-        y = s.geometry.y,
-        ontop = false,
-        visible = true,
-        --[[shape = function(cr,w,h)
+	s.mytags = wibox({
+		screen = s,
+		width = 100,
+		height = 25,
+		x = s.geometry.x + 2 * beautiful.useless_gap,
+		y = s.geometry.y,
+		ontop = false,
+		visible = true,
+		--[[shape = function(cr,w,h)
             gears.shape.rounded_rect(cr,w,h,6)
         end,--]]
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal
-    }
-    s.mytags:setup {
-        s.mytaglist,
-        margins = beautiful.useless_gap,
-        widget = wibox.container.margin,
-    }
-    s.mytags:struts {
-        top = s.mytags.height + s.mytags.y + beautiful.useless_gap
-    }
+		border_width = beautiful.border_width,
+		-- border_color = beautiful.border_normal,
+		bg = "#00000000",
+	})
+	s.mytags:setup({
+		s.mytaglist,
+		margins = beautiful.useless_gap,
+		widget = wibox.container.margin,
+	})
+	s.mytags:struts({
+		top = s.mytags.height + s.mytags.y,
+	})
 
-    s.mytasks = wibox {
-        screen = s,
-        width = 200,
-        height = s.mytags.height,
-        x = s.mytags.x + s.mytags.width + (beautiful.useless_gap + (2 * beautiful.border_width)),
-        y = s.mytags.y,
-        ontop = false,
-        visible = true,
-        --[[shape = function(cr,w,h)
+	s.mytasks = wibox({
+		screen = s,
+		width = 200,
+		height = s.mytags.height,
+		x = s.mytags.x + s.mytags.width + (beautiful.useless_gap + (2 * beautiful.border_width)),
+		y = s.mytags.y,
+		ontop = false,
+		visible = true,
+		--[[shape = function(cr,w,h)
             gears.shape.rounded_rect(cr,w,h,6)
         end,--]]
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal
-    }
-    s.mytasks:setup {
-        s.mytasklist,
-        margins = beautiful.useless_gap,
-        widget = wibox.container.margin
-    }
+		border_width = beautiful.border_width,
+		-- border_color = beautiful.border_normal,
+		bg = "#00000000",
+	})
+	s.mytasks:setup({
+		s.mytasklist,
+		margins = beautiful.useless_gap,
+		widget = wibox.container.margin,
+	})
 
-    s.mylayout = wibox {
-        screen = s,
-        width = s.mytags.height,
-        height = s.mytags.height,
-        x = s.geometry.x + s.geometry.width - s.mytags.height - (2 * beautiful.border_width) - (2 * beautiful.useless_gap),
-        y = s.mytags.y,
-        ontop = false,
-        visible = true,
-        --[[shape = function(cr,w,h)
+	s.mylayout = wibox({
+		screen = s,
+		width = s.mytags.height,
+		height = s.mytags.height,
+		x = s.geometry.x
+			+ s.geometry.width
+			- s.mytags.height
+			- (2 * beautiful.border_width)
+			- (2 * beautiful.useless_gap),
+		y = s.mytags.y,
+		ontop = false,
+		visible = true,
+		--[[shape = function(cr,w,h)
             gears.shape.rounded_rect(cr,w,h,6)
         end,--]]
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal,
-    }
-    s.mylayout:setup {
-        s.mylayoutbox,
-        margins = beautiful.useless_gap,
-        widget = wibox.container.margin
-    }
+		border_width = beautiful.border_width,
+		-- border_color = beautiful.border_normal,
+		bg = "#00000000",
+	})
+	s.mylayout:setup({
+		s.mylayoutbox,
+		margins = beautiful.useless_gap,
+		widget = wibox.container.margin,
+	})
 
-    s.systemutilities = wibox {
-        screen = s,
-        width = 300,
-        height = s.mytags.height,
-        x = s.mylayout.x - (300 + beautiful.useless_gap + (2 * beautiful.border_width)),
-        y = s.mytags.y,
-        ontop = false,
-        visible = true,
-        --[[shape = function(cr,w,h)
+	s.systemutilities = wibox({
+		screen = s,
+		width = 300,
+		height = s.mytags.height,
+		x = s.mylayout.x - (300 + beautiful.useless_gap + (2 * beautiful.border_width)),
+		y = s.mytags.y,
+		ontop = false,
+		visible = true,
+		--[[shape = function(cr,w,h)
             gears.shape.rounded_rect(cr,w,h,6)
         end,--]]
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal,
-    }
-    s.systemutilities:setup {
-        {
-            {
-                wibox.widget.systray(),
-                {
-                    cpu.widget,
-                    cpu_temp.widget,
-                    layout = wibox.layout.fixed.horizontal,
-                    spacing = 8
-                },
-                bat.widget,
-                widget = wibox.container.place,
-                spacing = 16,
-                spacing_widget = wibox.widget.separator,
-                layout = wibox.layout.fixed.horizontal
-            },
-            halign = "right",
-            widget = wibox.container.place
-        },
-        top = beautiful.useless_gap,
-        bottom = beautiful.useless_gap,
-        left = 6,
-        right = 6,
-        widget = wibox.container.margin
-    }
+		border_width = beautiful.border_width,
+		-- border_color = beautiful.border_normal,
+		bg = "#00000000",
+	})
+	s.systemutilities:setup({
+		{
+			{
+				wibox.widget.systray(),
+				{
+					cpu.widget,
+					cpu_temp.widget,
+					layout = wibox.layout.fixed.horizontal,
+					spacing = 8,
+				},
+				bat.widget,
+				widget = wibox.container.place,
+				spacing = 16,
+				spacing_widget = wibox.widget.separator,
+				layout = wibox.layout.fixed.horizontal,
+			},
+			halign = "right",
+			widget = wibox.container.place,
+		},
+		top = beautiful.useless_gap,
+		bottom = beautiful.useless_gap,
+		left = 6,
+		right = 6,
+		widget = wibox.container.margin,
+	})
 
-    s.mydatetime = wibox {
-        screen = s,
-        width = s.systemutilities.x -
-            (s.mytasks.x + s.mytasks.width) -
-            (2 * beautiful.useless_gap) -
-            (4 * beautiful.border_width),
-        --width = 200,
-        height = s.mytags.height,
-        x = s.mytasks.x + s.mytasks.width + (beautiful.useless_gap + (2 * beautiful.border_width)),
-        --x = (s.geometry.width / 2) - (200 / 2),
-        y = s.mytags.y,
-        ontop = false,
-        visible = true,
-        bg = beautiful.bg_normal,--"#FFFFFF00",
-        fg = beautiful.bg_focus,
-        border_width = beautiful.border_width,
-        border_color = beautiful.border_normal,
-    }
-    s.mydatetime:setup {
-        {
-            mytextclock,
-            layout = wibox.layout.flex.horizontal
-        },
-        widget = wibox.container.place
-    }
+	s.mydatetime = wibox({
+		screen = s,
+		width = s.systemutilities.x
+			- (s.mytasks.x + s.mytasks.width)
+			- (2 * beautiful.useless_gap)
+			- (4 * beautiful.border_width),
+		--width = 200,
+		height = s.mytags.height,
+		x = s.mytasks.x + s.mytasks.width + (beautiful.useless_gap + (2 * beautiful.border_width)),
+		--x = (s.geometry.width / 2) - (200 / 2),
+		y = s.mytags.y,
+		ontop = false,
+		visible = true,
+		bg = "#00000000", --beautiful.bg_normal,
+		fg = beautiful.bg_focus,
+		border_width = beautiful.border_width,
+		-- border_color = beautiful.border_normal,
+		bg = "#00000000",
+	})
+	s.mydatetime:setup({
+		{
+			mytextclock,
+			layout = wibox.layout.flex.horizontal,
+		},
+		widget = wibox.container.place,
+	})
 
-    s.mypromptpopup = awful.popup {
-        screen = s,
-        minimum_height = s.mytasks.height,
-        maximum_height = s.mytasks.height,
-        x = s.mytasks.x + s.mytasks.width + (beautiful.useless_gap + (3 * beautiful.border_width)),
-        y = s.mytasks.y + beautiful.border_width,
-        ontop = false,
-        visible = true,
-        --[[shape = function(cr,w,h)
+	s.mypromptpopup = awful.popup({
+		screen = s,
+		minimum_height = s.mytasks.height,
+		maximum_height = s.mytasks.height,
+		x = s.mytasks.x + s.mytasks.width + (beautiful.useless_gap + (3 * beautiful.border_width)),
+		y = s.mytasks.y + beautiful.border_width,
+		ontop = false,
+		visible = true,
+		--[[shape = function(cr,w,h)
             gears.shape.rounded_rect(cr,w,h,6)
         end,--]]
-        widget = {
-            s.mypromptbox,
-            margins = beautiful.useless_gap,
-            widget = wibox.container.margin
-        }
-    }
-
+		bg = "#00000000",
+		widget = {
+			s.mypromptbox,
+			margins = beautiful.useless_gap,
+			widget = wibox.container.margin,
+		},
+	})
 end
 
 return bars
