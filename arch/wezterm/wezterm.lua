5f4730d7b72e6a4d9a60687d3e7accd3cdd09a60
taoaustin <taoaustin@yahoo.ca>
2025-06-25 02:24:37 -0400

    wezterm

---
+++ arch/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "melange_dark"
+config.window_background_opacity = 0.96
+config.font = wezterm.font("0xProto Nerd Font Mono")
+config.font_size = 12
+
+return config
